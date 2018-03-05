16573cb5f1c1f6d967607c0f5049131384ac9121
Anorlondo448 <kanaend0605@gmail.com>
2018-03-06 07:13:24 +0900

    Lambdaのhandlerの定義が間違っていたので修正

--- apigateway_lambda_batch/lambda_function.tf
+++ apigateway_lambda_batch/lambda_function.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "mokumoku" {
   filename         = "lambda_function_payload.zip"
   function_name    = "lambda_function_name"
   role             = "${aws_iam_role.lambda_role.arn}"
-  handler          = "exports.test"
+  handler          = "lambda_function_payload.lambda_handler"
   source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
   runtime          = "python3.6"
 
