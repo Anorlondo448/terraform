649bdf3619e858de2e7fbf729b03c62bdca231af
Anorlondo448 <kanaend0605@gmail.com>
2018-03-06 06:28:19 +0900

    Lambda用のIAM Role追加

---
+++ apigateway_lambda_batch/iam_lambda.tf
@@ -0,0 +1,23 @@
+###
+#
+# IAM for Lambda
+#
+# AssumeRoleをLambdaにしたIAMロール作成
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
