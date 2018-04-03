7d219cb8cfc18f53fab60df64dce15f296a379a3
Anorlondo448 <kanaend0605@gmail.com>
2018-04-04 07:13:46 +0900

    cloudwatch logsのIAMロール設定をコメントアウト - AWSコンソールから実施するとIAMロールの紐付けと別に、独自のポリシー(oneClick_Cloudtrail...)の紐付けを要求される - 独自のポリシーの操作がterraformから難しいので、AWSコンソールから作成する

--- cloudtrail_to_elasticsearch/cloudtrail.tf
+++ cloudtrail_to_elasticsearch/cloudtrail.tf
@@ -8,4 +8,10 @@ resource "aws_cloudtrail" "all-region" {
   enable_log_file_validation    = true
   include_global_service_events = true
   is_multi_region_trail         = true
+
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail.arn}"
+
+  # AWSコンソールから実施するとIAMロールの紐付けと別に、独自のポリシー(oneClick_Cloudtrail...)の紐付けを要求される
+  # 独自のポリシーの操作がterraformから難しいので、AWSコンソールから作成する
+  # cloud_watch_logs_role_arn  = "${aws_iam_role.cloudtrail.arn}"
 }
