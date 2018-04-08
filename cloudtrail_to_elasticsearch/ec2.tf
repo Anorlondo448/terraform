75aae491ea5aadfc71704c28af97b5af1157ffde
Anorlondo448 <kanaend0605@gmail.com>
2018-04-09 00:22:12 +0900

    change Amazon Linux 2

--- cloudtrail_to_elasticsearch/ec2.tf
+++ cloudtrail_to_elasticsearch/ec2.tf
@@ -33,7 +33,7 @@ data "aws_ami" "amazon_linux" {
 }
 
 resource "aws_instance" "bastion" {
-  ami                         = "${data.aws_ami.amazon_linux.id}"
+  ami                         = "ami-c2680fa4"                       // Amazon Linux 2
   instance_type               = "${var.instance-type}"
   vpc_security_group_ids      = ["${aws_security_group.bastion.id}"]
   subnet_id                   = "${aws_subnet.public.id}"
