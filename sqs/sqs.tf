9604f731d57304291d685e70bed9a524f1ff33e8
Anorlondo448 <kanaend0605@gmail.com>
2019-05-11 22:15:10 +0900

    add default sqs setting

--- sqs/sqs.tf
+++ sqs/sqs.tf
@@ -4,5 +4,9 @@
 #
 
 resource "aws_sqs_queue" "sample_standard" {
-  name = "sample_standard_queue"
+  name                      = "sample_standard_queue"
+  delay_seconds             = "0"
+  max_message_size          = "262144"
+  message_retention_seconds = "345600"
+  receive_wait_time_seconds = "0"
 }
