cac8f3bdb97e6417b637c06e9f70d9dd7b71259f
Anorlondo448 <kanaend0605@gmail.com>
2019-05-11 22:09:09 +0900

    terraform fmt

--- sqs/config.tf
+++ sqs/config.tf
@@ -5,9 +5,9 @@
 
 terraform {
   backend "s3" {
-    bucket  = "anorlondo448-terraform"
-    key     = "sqs/terraform.tfstate"
-    region  = "ap-northeast-1"
+    bucket = "anorlondo448-terraform"
+    key    = "sqs/terraform.tfstate"
+    region = "ap-northeast-1"
   }
 
   required_version = ">= 0.11.13"
